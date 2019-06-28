caffa11d7f4fbb7c373bbd290f476dad9256336e
Mozyanari <mozyanari@outlook.jp>
2019-06-28 21:10:17 +0900

    pgmのデータ以外は取得出来た

--- pgm_edit/src/pgm_test.cpp
+++ pgm_edit/src/pgm_test.cpp
@@ -15,6 +15,10 @@ private:
 
     //ノードハンドラ作成
     ros::NodeHandle nh;
+
+    int row = 0, col = 0, numrows = 0, numcols = 0;
+    int **array;
+    int grey_max;
 };
 
 //コンストラクタ
@@ -24,11 +28,64 @@ pgm_test::pgm_test(){
     std::stringstream ss;
     std::string inputline = "";
 
+    //pgm形式は
+    //1.マジックナンバーP2 or P5
+    //2.コメント
+    //3.データの行列の数
+    //4.グレースケールの最大値
+    //5.データ(P5は1byteずつ)
+
     //バージョンを吐き出す
     //infileの中の行末までを読みだす
     std::getline(infile,inputline);
     ROS_INFO("%s",inputline);
     std::cout << inputline << std::endl;
+
+    // Second line : comment
+    std::getline(infile,inputline);
+    std::cout << "Comment : " << inputline << std::endl;
+
+    /*
+    std::getline(infile,inputline);
+    std::cout << "row and line : " << inputline << std::endl;
+
+    std::getline(infile,inputline);
+    std::cout << "data_max : " << inputline << std::endl;
+
+    std::getline(infile,inputline);
+    std::cout << "data : " << inputline << std::endl;
+    */
+    // Continue with a stringstream
+    ss << infile.rdbuf();
+    // Third line : size
+    //ssは空白で区切られて書き込むから，for文みたいに増やしていく必要はない
+    ss >> numcols >> numrows;
+    std::cout << numcols << " columns and " << numrows << " rows" << std::endl;
+
+    // fourth line ; data_max
+    ss >> grey_max;
+    std::cout << "grey max" << grey_max << std::endl;
+
+    numcols = 10;
+    numrows = 10;
+    array = new int*[numrows];
+    for(int i = 0; i< numrows;++i){
+        array[i] = new int [numcols];
+    }
+
+    
+    for(row = 0; row < numrows; ++row){
+        for (col = 0; col < numcols; ++col){
+            ss >> array[row][col];
+            //array[row][col] = fgetc(ss);
+        }
+    }
+    for(row = 0; row < numrows; ++row) {
+        for(col = 0; col < numcols; ++col) {
+            std::cout << array[row][col] << std::endl;
+        }
+    }
+
 }
 
 //関数定義-----------------------------------------------------------------------
