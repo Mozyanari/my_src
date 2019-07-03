02a885c2be91c4844a175e99177d774b9be7ffb3
mozyanari_home <mozyanari@outlook.jp>
2019-07-03 20:56:38 +0900

    ifstreamを使用した形式を削除

--- pgm_edit/src/pgm_test.cpp
+++ pgm_edit/src/pgm_test.cpp
@@ -35,16 +35,22 @@ pgm_test::pgm_test(){
     //usernameを取得
     char *username;
     username = std::getenv("USER");
-    std::cout << username << std::endl;
+    //std::cout << username << std::endl;
 
     //mapのパスは/home/(username)/maps/map_test.pgmにある前提
     std::string home = "/home/";
     std::string map = "/maps/map_test.pgm";
     //パスを結合
     std::string filename = home + username + map;
-    std::cout << filename << std::endl;
+    //std::cout << filename << std::endl;
 
     //mapをオープンして読み込み
+    //pgm形式は
+    //1.マジックナンバーP2 or P5
+    //2.コメント
+    //3.データの行列の数
+    //4.グレースケールの最大値
+    //5.データ(P5は1byteずつ)
     fp = fopen(filename.c_str(),"rb");
     //fp = fopen("/home/masanari/maps/map_test.pgm","rb");
 
@@ -53,10 +59,16 @@ pgm_test::pgm_test(){
     //fpからcharバイトのデータを3つ読む
     //読みだすたびにfpの指定位置が変化する
     fread(type,sizeof(char),3,fp);
-    
+    type[3] = '\n';
+    //mapデータのpgmのはずなので、P5の場合は実行しない
+    if(strcmp("P5\n",type) != 0){
+        return;
+    }
+    /*
     for(int i=0;i<3;i++){
         std::cout << type[i] << std::endl;
     }
+    */
     //出力結果
     //P5
     //
@@ -92,7 +104,6 @@ pgm_test::pgm_test(){
     //白は254
     //黒は0
     //グレーは205
-
     for(int i=0;i<width;i++){
         for(int j=0;j<height;j++)
         {
@@ -129,74 +140,6 @@ pgm_test::pgm_test(){
         std::cout << ex.what() << std::endl;
         throw;
     }
-
-    /*
-    //どのファイルを読むか指定
-    std::ifstream infile("/home/masanari/maps/map_test.pgm");
-    std::stringstream ss;
-    std::string inputline = "";
-
-    //pgm形式は
-    //1.マジックナンバーP2 or P5
-    //2.コメント
-    //3.データの行列の数
-    //4.グレースケールの最大値
-    //5.データ(P5は1byteずつ)
-
-    //バージョンを吐き出す
-    //infileの中の行末までを読みだす
-    std::getline(infile,inputline);
-    ROS_INFO("%s",inputline);
-    std::cout << inputline << std::endl;
-
-    // Second line : comment
-    std::getline(infile,inputline);
-    std::cout << "Comment : " << inputline << std::endl;
-    */
-
-    /*
-    std::getline(infile,inputline);
-    std::cout << "row and line : " << inputline << std::endl;
-
-    std::getline(infile,inputline);
-    std::cout << "data_max : " << inputline << std::endl;
-
-    std::getline(infile,inputline);
-    std::cout << "data : " << inputline << std::endl;
-    */
-
-   /*
-    // Continue with a stringstream
-    ss << infile.rdbuf();
-    // Third line : size
-    //ssは空白で区切られて書き込むから，for文みたいに増やしていく必要はない
-    ss >> numcols >> numrows;
-    std::cout << numcols << " columns and " << numrows << " rows" << std::endl;
-
-    // fourth line ; data_max
-    ss >> grey_max;
-    std::cout << "grey max" << grey_max << std::endl;
-
-    numcols = 10;
-    numrows = 10;
-    array = new int*[numrows];
-    for(int i = 0; i< numrows;++i){
-        array[i] = new int [numcols];
-    }
-
-    
-    for(row = 0; row < numrows; ++row){
-        for (col = 0; col < numcols; ++col){
-            ss >> array[row][col];
-            //array[row][col] = fgetc(ss);
-        }
-    }
-    for(row = 0; row < numrows; ++row) {
-        for(col = 0; col < numcols; ++col) {
-            std::cout << array[row][col] << std::endl;
-        }
-    }
-    */
 
 }
 
